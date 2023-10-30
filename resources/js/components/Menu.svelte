f18618f11be562fc527c6f5c8b4b4e51cba147a8
TENMAJKL <silder.hologram@gmail.com>
2023-10-30 12:55:10 +0100

    removes text on mobile devices

--- resources/js/components/Menu.svelte
+++ resources/js/components/Menu.svelte
@@ -10,7 +10,7 @@
     <div class="flex flex-col items-center text-xs md:flex-row md:gap-2 md:text-sm">
         <div class="{selected === index ? 'text-blue' : ''} flex flex-col md:flex-row md:gap-2 items-center transition ease-in-out" on:click={() => selected = index}>
             <i class="bi bi-{icon} text-2xl" />
-            <Text text="{item}" />
+            <span class="hidden sm:block"><Text text="{item}" /></span>
         </div>
     </div>
 {/each}
