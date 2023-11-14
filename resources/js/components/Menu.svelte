65ba5c693ff924c5dc43b728397405b9327d0dc9
TENMAJKL <silder.hologram@gmail.com>
2023-11-14 14:14:53 +0100

    cursor pointer

--- resources/js/components/Menu.svelte
+++ resources/js/components/Menu.svelte
@@ -7,7 +7,7 @@
 </script>
 
 {#each items as [item, _, icon], index}
-    <div class="flex flex-col items-center text-xs md:flex-row md:gap-2 md:text-sm">
+    <div class="flex flex-col items-center text-xs md:flex-row md:gap-2 md:text-sm {selected == index ? 'cursor-default' : 'cursor-pointer'}">
         <div class="{selected === index ? 'text-blue' : ''} flex flex-col md:flex-row md:gap-2 items-center transition ease-in-out" on:click={() => selected = index}>
             <i class="bi bi-{icon} text-2xl" />
             <span class="hidden sm:block"><Text text="{item}" /></span>
